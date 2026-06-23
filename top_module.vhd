b23d6a17c6fda68f79675e441d2c2f22fc370548
Ahmet Alper DAĞ <ahmetalper.dag@gmail.com>
2026-06-23 16:22:30 +0300

    Update top_module.vhd

--- top_module.vhd
+++ top_module.vhd
@@ -1,91 +1,73 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 entity top_module is
     Port (
-        clk             : in  std_logic; -- FPGA üzerindeki 100 MHz saat pinine baglanacak
-        reset           : in  std_logic; -- Reset butonuna baglanacak
-        start_sweep_i   : in  std_logic; -- Tarama baslatma butonuna baglanacak
+        clk             : in  std_logic;
+        reset           : in  std_logic;
+        start_sweep_i   : in  std_logic;
         
-        -- SPI Portlari (DAC Baglantisi için fiziksel pinler)
         sclk            : out std_logic;
         cs              : out std_logic;
         mosi            : out std_logic;
         
-        -- XADC Portlari (Analog Giris pinleri)
         vauxp3          : in  std_logic;
         vauxn3          : in  std_logic;
         
-        -- UART Port (PC Baglantisi için TX pini)
         tx_pin          : out std_logic
     );
 end top_module;
 
 architecture Behavioral of top_module is
 
-    -- 1. MODULLER ARASI BAGLANTI SINYALLERI (Kablolama)
-    
-    -- SPI Hatlari
     signal spi_start_s    : std_logic;
     signal spi_busy_s     : std_logic;
     signal dac_data_s     : std_logic_vector(11 downto 0);
     
-    -- XADC Hatlari
     signal start_adc_s    : std_logic;
     signal data_ready_s   : std_logic;
     signal adc_raw_s      : std_logic_vector(11 downto 0);
     
-    -- DBM_CALC Hatlari
     signal dbm_val_s      : std_logic_vector(31 downto 0);
     signal calc_ready_s   : std_logic;
     
-    -- UART Hatlari
     signal uart_data_s    : std_logic_vector(7 downto 0);
     signal tx_start_s     : std_logic;
     signal tx_busy_s      : std_logic;
 
 begin
 
-    -- 2. MODULLERIN YERLESTIRILMESI (Instantiation)
-
-    -- ANA KONTROL MERKEZI (FSM)
-    -- Butun trafigi bu arkadas yonetiyor.
     i_main_fsm : entity work.main_fsm
     port map (
         clk          => clk,
         reset        => reset,
         start_sweep  => start_sweep_i,
-        -- SPI (DAC) Baglantilari
         spi_start    => spi_start_s,
         dac_data     => dac_data_s,
         spi_busy     => spi_busy_s,
-        -- XADC Baglantilari
         start_adc    => start_adc_s,
         data_ready   => data_ready_s,
-        -- dBm_calc Baglantilari
         dbm_data_i   => dbm_val_s,
         calc_ready   => calc_ready_s,
-        -- UART Baglantilari
         uart_data_o  => uart_data_s,
         tx_start     => tx_start_s,
         tx_busy      => tx_busy_s,
-        sweep_active => open -- Kullanilmiyor
+        sweep_active => open
     );
 
-    -- SPI MASTER (DAC Kontrol)
     i_spi_master : entity work.spi_master
     port map (
-        clk      => clk,
-        reset    => reset,
-        spi_start=> spi_start_s,
-        dac_data => dac_data_s,
-        sclk     => sclk,
-        cs       => cs,
-        mosi     => mosi,
-        spi_busy => spi_busy_s
+        clk       => clk,
+        reset     => reset,
+        spi_start => spi_start_s,
+        dac_data  => dac_data_s,
+        sclk      => sclk,
+        cs        => cs,
+        mosi      => mosi,
+        spi_busy  => spi_busy_s
     );
 
-    -- XADC MODULU (Analog Okuma)
     i_xadc_module : entity work.xadc_module
     port map (
         clk          => clk,
@@ -97,18 +79,16 @@ begin
         vauxn3       => vauxn3
     );
 
-    -- DBM_CALC (LUT tablosu ile dbm donusumu)
     i_dbm_calc : entity work.dbm_calc
     port map (
-        clk          => clk,
-        reset        => reset,
-        xadc_data    => adc_raw_s,
-        xadc_ready   => data_ready_s,
-        dbm_val      => dbm_val_s,
-        dbm_ready    => calc_ready_s
+        clk        => clk,
+        reset      => reset,
+        xadc_data  => adc_raw_s,
+        xadc_ready => data_ready_s,
+        dbm_val    => dbm_val_s,
+        dbm_ready  => calc_ready_s
     );
 
-    -- UART TX (Verileri PC'ye seri hattan gonderir)
     i_uart_tx : entity work.uart_tx
     port map (
         clk      => clk,
